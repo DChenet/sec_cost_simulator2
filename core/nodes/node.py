dbec1133d5af7b25afe00af138f87685a977e50b
Dorian Chenet <chenetdorian@gmail.com>
2025-11-12 10:12:12 +0100

    new n cost

--- core/nodes/node.py
+++ core/nodes/node.py
@@ -1,12 +1,12 @@
-
+import numpy as np
 
 class Node:
     def __init__(self, speed: float, phi: float):
         self.speed = speed
-        self.phi = phi
+        self.phi = np.float128(phi)
 
     def process(self, data: int) -> int:
-        return int(data * self.phi)
+        return int(round(np.ceil(np.float128(data) * self.phi)))
 
     def get_speed(self) -> float:
         return self.speed
@@ -15,7 +15,7 @@ class Node:
         self.speed = speed
 
     def get_phi(self) -> float:
-        return self.speed
+        return self.phi  # Fixed: was returning self.speed
 
     def set_phi(self, phi: float) -> None:
-        self.speed = phi
+        self.phi = phi  # Fixed: was setting self.speed
